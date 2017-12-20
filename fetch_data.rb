e7a6bbc3d4c50437b68daba9ab09833f73530dbd
Buraga Dmitrii <buraga.dmitrii@gmail.com>
2017-12-20 11:35:27 +0200

    Refactoring Service class

--- fetch_data.rb
+++ fetch_data.rb
@@ -1,17 +1,14 @@
 require './service'
 
+login, password = Service.user_input_credentials
 
-login, password = Service::user_input_credentials
+session = Service.login_with(login, password)
 
-session = Service::login_with(login, password)
+accounts     = Service.get_accounts(session)
+transactions = Service.get_transactions(session)
 
-accounts     = Service::get_accounts(session)
-transactions = Service::get_transactions(session)
-
-code = Service::logout(session)
+code = Service.logout(session)
 
 ap accounts
 
 ap transactions
-
-
--- service.rb
+++ service.rb
@@ -7,119 +7,86 @@ require 'rest-client'
 require './account'
 require './transaction'
 
-
-class Service 
-
-  BASE_MICB_URL     = "https://wb.micb.md/way4u-wb2/api/v2"
-  LOGIN_URL         = "#{BASE_MICB_URL}/session"
-  ACCOUNTS_URL      = "#{BASE_MICB_URL}/contracts"
-  TRANSACTIONS_URL  = "#{BASE_MICB_URL}/history?from=2017-11-13"  
-  DATA = { 
-           accounts: ACCOUNTS_URL,
-           transactions: TRANSACTIONS_URL
-         }
+class Service
+  BASE_MICB_URL     = 'https://wb.micb.md/way4u-wb2/api/v2'.freeze
+  LOGIN_URL         = "#{BASE_MICB_URL}/session".freeze
+  ACCOUNTS_URL      = "#{BASE_MICB_URL}/contracts".freeze
+  TRANSACTIONS_URL  = "#{BASE_MICB_URL}/history?from=2017-11-13".freeze
+  FETCHING_DATA = {
+    accounts: ACCOUNTS_URL,
+    transactions: TRANSACTIONS_URL
+  }.freeze
 
   def self.user_input_credentials
-    print "Login for your MICB account: "
+    print 'Login for your MICB account: '
     login    = gets.chomp
-    password = STDIN.getpass("Password for your MICB account: ").chomp
+    password = STDIN.getpass('Password for your MICB account: ').chomp
     [login, password]
   end
 
-
   def self.login_with(login, password)
     puts 'Trying to login...'
-    begin
-      response = RestClient.post LOGIN_URL,
-              {"login" => login,
-               "password" => password,
-               "captcha" => ""}.to_json,
-              {content_type: :json, accept: :json}
-      
-    rescue RestClient::ExceptionWithResponse => e
-      if e.response.code == 412
-        abort "Invalid Login or Password"
-      end
-    rescue StandardError => e
-      binding.pry  
-      p e  
-    else
-      cookies = response.cookies  
-    end  
-
-    response
+    request {
+      RestClient.post LOGIN_URL,
+                      { 'login' => login,
+                        'password' => password,
+                        'captcha' => '' }.to_json,
+                      content_type: :json, accept: :json
+    }
   end
 
-
   def self.fetch_data(session, info_type)
-    puts 'Fetching data...'  
-    begin
-      response = RestClient.get DATA[info_type], {:cookies => session.cookies}
-    rescue RestClient::ExceptionWithResponse => e
-      case e.response.code
-      when 405
-        abort "Not Allowed"
-      when 412
-        abort "Invalid Login or Password"
-      end
-    rescue StandardError => e
-      binding.pry  
-      p e  
-    end  
+    puts "Fetching #{info_type}..."
+    response = request {
+      RestClient.get FETCHING_DATA[info_type],
+                     cookies: session.cookies
+    }
     JSON.parse(response.body)
   end
 
-
   def self.logout(session)
     puts 'Loging out...'
-    begin
-      response = RestClient.delete LOGIN_URL, {:cookies => session.cookies}
-    rescue RestClient::ExceptionWithResponse => e
-      if e.response.code == 412
-        abort "Invalid Login or Password"
-      end
-    rescue StandardError => e
-      binding.pry  
-    end  
-
+    response = request { RestClient.delete LOGIN_URL, cookies: session.cookies }
     response.code
-  end  
+  end
 
   def self.get_accounts(session)
-    raw_accounts = Service::fetch_data(session, :accounts)
-    accounts = []
+    raw_accounts = Service.fetch_data(session, :accounts)
     raw_accounts.map do |raw_account|
-
       account = Account.new
-      account.name        = raw_account["number"]
-      account.balance     = raw_account["balances"]["available"]["value"]
-      account.currency    = raw_account["balances"]["available"]["currency"]
-      account.description = raw_account["number"]
-
-      accounts << account
+      account.name        = raw_account['number']
+      account.balance     = raw_account['balances']['available']['value']
+      account.currency    = raw_account['balances']['available']['currency']
+      account.description = raw_account['number']
+      account
     end
-
-    accounts
-  end  
+  end
 
   def self.get_transactions(session)
-    raw_transactions = Service::fetch_data(session, :transactions)
-    transactions = []
+    raw_transactions = Service.fetch_data(session, :transactions)
     raw_transactions.map do |raw_transaction|
       transaction = Transaction.new
-      transaction.date        = raw_transaction["effectiveFrom"]
-      transaction.description = 
-           if raw_transaction["service"]
-            raw_transaction["service"]["name"]
-           else  
-            raw_transaction["description"]
-           end 
-      transaction.amount      = raw_transaction["totalAmount"]["value"]
-
-      transactions << transaction
+      transaction.date        = raw_transaction['operationTime']
+      transaction.description = raw_transaction['service'] ? raw_transaction['service']['name'] : raw_transaction['description']
+      transaction.amount      = raw_transaction['totalAmount']['value']
+      transaction
     end
+  end
 
-    transactions
-  end    
-  
+  private
+
+  def self.request
+    yield
+    rescue RestClient.ExceptionWithResponse => e
+      case e.response.code
+      when 405
+        abort 'Access denied'
+      when 412
+        abort 'Invalid Login or Password'
+      end
+    rescue SocketError => e
+      abort "Can't connect to the service"
+    rescue StandardError => e
+      abort "Error: #{e.message}"
+    end
 end
